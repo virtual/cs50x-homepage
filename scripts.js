f5d1f217a3609aeada7aee7a58c817320464087b
Jeanine Schoessler <jeanine.mt@gmail.com>
2021-11-14 16:48:50 -0700

    add content

--- scripts.js
+++ scripts.js
@@ -1,3 +1,38 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
+    let grid = document.querySelectorAll(".image-grid.homepage-grid > div");
+    if (grid) {
+       loadImages(grid, 'artists', 'jpg');
+    }
+    todaysDate = new Date();
+    todaysDate.setHours(0, 0, 0, 0);
+    pickedDatestr = "19-Nov-2021"
+    var pickedDate = new Date(Date.parse(pickedDatestr.replace(/-/g, " ")));
+    if (pickedDate <= todaysDate) {
+        let alert = document.querySelector(".alert.d-none");
+        let fgrid = document.querySelectorAll(".image-grid.image-smile > div");
+        if (fgrid) {
+            loadImages(fgrid, 'f3');
+        }
+        if (alert) {
+            alert.classList.remove("d-none");
+        }
+    }
 });
+
+function loadImages(grid, path, ext="png") {
+    let i = 0;
+    for (const griditem of grid) {
+        if (griditem.classList.contains('active')) { 
+            griditem.dataset.imgid = i;
+            griditem.dataset.imgpath = path;
+            i++;
+
+            griditem.addEventListener('click', function(event) {
+                showImg(this, griditem.dataset.imgid, griditem.dataset.imgpath, ext)
+            });
+        }
+    }
+} 
+function showImg(el, id, path, ext) {
+    el.style.backgroundImage = `url('./resources/images/${path}/${id}.${ext}')`;
+}
